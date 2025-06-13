8508fc3f60da201a712135be6cbdff2fcb2d28a2
Caio Harrys <blackiaratanmusic@gmail.com>
2025-06-13 03:30:03 -0300

    navbar feita e algumas alterações no codigo HTML E JS

--- assets/js/jay.js
+++ assets/js/jay.js
@@ -1,13 +1,30 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('show');
+        entry.target.classList.add("show");
       }
     });
-  }, {
-    threshold: 0.1
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+document.querySelectorAll(".fade-in").forEach((el) => observer.observe(el));
+
+console.log("harrys passou aqui");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+  // Ativa o primeiro por padrão
+  navLinks[0].classList.add("active");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      navLinks.forEach((l) => l.classList.remove("active")); // Remove todos
+      this.classList.add("active"); // Ativa o clicado
+    });
   });
-
-  document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
-
-console.log('harrys passou aqui');
+});
